fa146013a5d4a223718c3bf4f6279de88c48c018
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-03-06 16:02:57 -0300

    Interface PostagemRepository

---
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -0,0 +1,9 @@
+﻿package com.generation.blogpessoal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.blogpessoal.model.Postagem;
+
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+	
+}
