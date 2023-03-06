031911b0cbb03e56c292887b635be50381d8385a
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-03-06 16:27:03 -0300

    Classe PostagemController

--- blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,9 +1,14 @@
 ﻿package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.generation.blogpessoal.model.Postagem;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 	
+    List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+    
 }
