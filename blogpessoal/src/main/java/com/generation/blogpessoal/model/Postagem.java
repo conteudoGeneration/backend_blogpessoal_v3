de97481c27166b02f5ca2d7de6b005d8328c6555
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-03-07 03:15:53 -0300

    Recurso Tema

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -4,10 +4,13 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -30,6 +33,10 @@ public class Postagem {
 	
 	@UpdateTimestamp
 	private LocalDateTime data;
+
+    @ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
     public Long getId() {
         return this.id;
@@ -63,4 +70,12 @@ public class Postagem {
         this.data = data;
     }
   
+    public Tema getTema() {
+        return this.tema;
+    }
+
+    public void setTema(Tema tema) {
+        this.tema = tema;
+    }
+
 }
