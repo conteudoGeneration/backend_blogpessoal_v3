73bcf52d435f3e92c898f15c4f7961904f2d54fa
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-03-07 03:26:19 -0300

    Recurso Usuario

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -38,6 +38,10 @@ public class Postagem {
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
 
+    @ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
+
     public Long getId() {
         return this.id;
     }
@@ -76,6 +80,14 @@ public class Postagem {
 
     public void setTema(Tema tema) {
         this.tema = tema;
+    }
+
+    public Usuario getUsuario() {
+        return this.usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
     }
 
 }
