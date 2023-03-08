6044742ed0abe84120aa98c49b417194e2e7e3ca
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-03-08 15:24:51 -0300

    Ambiente de Testes

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -42,6 +42,18 @@ public class Usuario {
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
 
+	/* Métodos Construtores */
+	
+	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+
+	public Usuario() { }
+
 	/* Insira os Getters and Setters */
 
 	public Long getId() {
